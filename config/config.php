aa14e4bd05ec3d658668cf064d8da9f0dd3ebe20
Victor Olubunmi <olubunmivictor6@gmail.com>
2023-11-28 11:36:40 +0100

    Improvements

--- config/config.php
+++ config/config.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 return [
 
     // Manage autoload migrations
@@ -9,7 +6,6 @@ return [
 
     // Subscriptions Database Tables
     'tables' => [
-
         'plans' => 'plans',
         'plan_features' => 'plan_features',
         'plan_subscriptions' => 'plan_subscriptions',
@@ -19,7 +15,6 @@ return [
 
     // Subscriptions Models
     'models' => [
-
         'plan' => \Rinvex\Subscriptions\Models\Plan::class,
         'plan_feature' => \Rinvex\Subscriptions\Models\PlanFeature::class,
         'plan_subscription' => \Rinvex\Subscriptions\Models\PlanSubscription::class,
